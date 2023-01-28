ddd71e4014808c376e01cdc1fd67ba354a7811b1
Adarsh <69946307+adarshswaminath@users.noreply.github.com>
2023-01-28 06:59:49 +0530

    Create SimpleCounter.py

---
+++ Python/SimpleCounter.py
@@ -0,0 +1,15 @@
+# Simple Counter Timer In Python
+
+import time
+def countdown(time_sec):
+    while time_sec:
+        mins, secs=divmod(time_sec, 60)
+        timeformat='{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+        print("stop")
+
+num=int(input("Set Your Timer in Sec: "))
+
+countdown(num)
